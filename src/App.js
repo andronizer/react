3fdae5e74140900574ee1ed8ecf0eb5994247f69
andronizer <olloloshik@mail.ru>
2021-08-18 16:58:22 +0300

    formik

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import {useFormik} from 'formik'
+import * as Yup from 'yup'
 
-function App() {
+const LoginForm = () => {
+  const {handleSubmit, handleChange, values, touched, errors, handleBlur} = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().max(10, 'Email must be shorter than 10 characters').required('Required'),
+      password: Yup.string().min(6, 'Password should be longer than 6 characters').required()
+    }),
+    onSubmit: ({email, password}) => {
+      alert(`Email: ${email}, password: ${password}`);
+    }
+  })
+
   return (
-    <div>
-      <h1>Hello, dear visitor!</h1>
-    </div>
+    <form onSubmit={handleSubmit}>      
+      <label htmlFor="email">Email</label>
+      <input
+        value={values.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        id="email"
+        name="email"
+        type="text"
+      />
+      {touched.email && errors.email ? (
+        <div>{errors.email}</div>
+      ): null}
+      <label htmlFor="password">Password</label>
+      <input
+        value={values.password}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        id="password"
+        name="password"
+        type="password"
+      />
+      {touched.password && errors.password ? (
+        <div>{errors.password}</div>
+      ): null}
+      <button type="submit">Log in</button>
+    </form>
   );
-}
+};
 
-export default App;
+export default LoginForm;
